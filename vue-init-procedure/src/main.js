7c27f20e85d10dbaf5cc7e5376afa5b47d6a0efe
boy-maofeiyu <1574552651@qq.com>
2021-08-22 09:46:09 +0800

    安装Vant组件库

--- vue-init-procedure/src/main.js
+++ vue-init-procedure/src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 // 加载全局样式文件index.less
 import './styles/index.less'
+// 加载vant组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 安装Vue插件
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
