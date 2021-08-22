8ad5c7f2191b22240d16ba5dfc4cdfe2e069ba66
boy-maofeiyu <1574552651@qq.com>
2021-08-22 15:47:00 +0800

    封装本地存储请求模块

---
+++ vue-init-procedure/src/utils/storage.js
@@ -0,0 +1,36 @@
+/**
+ * 本地存储操作封装
+ */
+
+/**
+ * 添加本地存储对象
+ */
+export const setItem = (name, value) => {
+  // 判断value类型
+  if (typeof value === 'Object') {
+    // 转换成JSON格式
+    value = JSON.stringify(value)
+  }
+  // 设置本地存储
+  window.localStorage.setItem(name, value)
+}
+
+/**
+ * 获取本地存储对象
+ */
+export const getItem = name => {
+  // 获取本地存储对象
+  const item = window.localStorage.getItem(name)
+  // 判断是否能转换成对象
+  try {
+    return JSON.parse(item)
+  } catch (e) {
+    return item
+  }
+}
+/**
+ * 删除本地存储对象
+ */
+export const removeItem = name => {
+  window.localStorage.removeItem(name)
+}
