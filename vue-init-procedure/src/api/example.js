15aa77050485024c15f10ebb59e7582d1463a28b
boy-maofeiyu <1574552651@qq.com>
2021-08-22 10:13:07 +0800

    封装请求模块及案例

---
+++ vue-init-procedure/src/api/example.js
@@ -0,0 +1,13 @@
+import request from "@/utils/request";
+
+/**
+ * api 案例
+ * @param data
+ */
+export const getUserInfo = (data) => {
+  request({
+    method: 'POST',
+    url: '',
+    data: data
+  })
+}
