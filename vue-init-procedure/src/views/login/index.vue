4cb445ca8031975857e8a1bdeec5aca1591603b4
boy-maofeiyu <1574552651@qq.com>
2021-08-22 14:09:51 +0800

    login界面布局

--- vue-init-procedure/src/views/login/index.vue
+++ vue-init-procedure/src/views/login/index.vue
@@ -1,10 +1,50 @@
 <template>
-  <div class="login-container"></div>
+  <div class="login-container">
+    <!-- nav-bar导航栏开始 -->
+    <van-nav-bar
+      title="注册/登录"
+      left-text="返回"
+      left-arrow
+      @click-left="this.$router.back()"
+    />
+    <!-- nav-bar导航栏结束 -->
+    <!-- 表单开始 -->
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        label="手机号"
+        left-icon="smile-o"
+        right-icon="warning-o"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        label="验证码"
+        left-icon="music-o"
+        placeholder="请输入验证码"
+      >
+        <!-- 发送验证码开始 -->
+        <template #button>
+          <van-button size="small" type="primary">获取验证码</van-button>
+        </template>
+        <!-- 发送验证码结束 -->
+      </van-field>
+    </van-cell-group>
+    <!-- 表单结束 -->
+    <!-- 登录按钮开始 -->
+    <van-button type="primary" size="large">登录</van-button>
+    <!-- 登录按钮结束 -->
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {}
+  },
+  methods: {}
 }
 </script>
 
