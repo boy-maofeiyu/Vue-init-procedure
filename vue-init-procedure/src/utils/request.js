b00d945cbd14e86fcbe21f609541424955bb0f1d
boy-maofeiyu <1574552651@qq.com>
2021-08-22 14:23:25 +0800

    封装login请求模块

--- vue-init-procedure/src/utils/request.js
+++ vue-init-procedure/src/utils/request.js
@@ -1,9 +1,9 @@
 /**
  * 封装axios请求模块
  */
-import axios from "axios";
+import axios from 'axios'
 
 const request = axios.create({
-  baseURL: ''  // 基础路径
+  baseURL: 'http://ttapi.research.itcast.cn' // 基础路径
 })
 export default request
--- vue-init-procedure/src/views/login/index.vue
+++ vue-init-procedure/src/views/login/index.vue
@@ -9,48 +9,67 @@
     />
     <!-- nav-bar导航栏结束 -->
     <!-- 表单开始 -->
-    <van-cell-group>
-      <van-field
-        v-model="user.mobile"
-        label="手机号"
-        left-icon="smile-o"
-        right-icon="warning-o"
-        placeholder="请输入手机号"
-      />
-      <van-field
-        v-model="user.code"
-        clearable
-        label="验证码"
-        left-icon="music-o"
-        placeholder="请输入验证码"
-      >
-        <!-- 发送验证码开始 -->
-        <template #button>
-          <van-button size="small" type="primary">获取验证码</van-button>
-        </template>
-        <!-- 发送验证码结束 -->
-      </van-field>
-    </van-cell-group>
+    <van-form @submit="onSubmit">
+      <van-cell-group>
+        <van-field
+          v-model="user.mobile"
+          label="手机号"
+          left-icon="smile-o"
+          right-icon="warning-o"
+          placeholder="请输入手机号"
+          :rules="formRules.mobileRules"
+        />
+        <van-field
+          v-model="user.code"
+          clearable
+          label="验证码"
+          left-icon="music-o"
+          placeholder="请输入验证码"
+          :rules="formRules.codeRules"
+        >
+          <!-- 发送验证码开始 -->
+          <template #button>
+            <van-button size="small" type="primary">获取验证码</van-button>
+          </template>
+          <!-- 发送验证码结束 -->
+        </van-field>
+      </van-cell-group>
+      <!-- 登录按钮开始 -->
+      <van-button type="primary" size="large">登录</van-button>
+      <!-- 登录按钮结束 -->
+    </van-form>
     <!-- 表单结束 -->
-    <!-- 登录按钮开始 -->
-    <van-button type="primary" size="large">登录</van-button>
-    <!-- 登录按钮结束 -->
   </div>
 </template>
 
 <script>
+import { login } from '../../api/user.js'
 export default {
   name: 'LoginIndex',
   data () {
     return {
       // 表单数据
       user: {
-        mobile: '17715319639', // 用户手机号
-        code: '245810' // 用户验证码
+        mobile: '13911111111', // 用户手机号
+        code: '246810' // 用户验证码
+      },
+      // 表单校验规则
+      formRules: {
+        mobileRules: [{ required: true, message: '请填写手机号' }],
+        codeRules: [{ required: true, message: '请填写验证码' }]
       }
     }
   },
-  methods: {}
+  methods: {
+    // 登录 表单提交事件
+    async onSubmit () {
+      try {
+        const { data } = await login(this.user)
+        console.log('登录成功', data)
+      } catch (e) {
+      }
+    }
+  }
 }
 </script>
 
