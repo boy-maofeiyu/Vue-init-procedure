5f31f112729b71940b21e0cfeb6abcb69a3fa99e
boy-maofeiyu <1574552651@qq.com>
2021-08-22 17:58:05 +0800

    my页面未登录页面布局

--- vue-init-procedure/src/views/my/index.vue
+++ vue-init-procedure/src/views/my/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="my-container">
-    <!-- 我的页面头部开始 -->
+    <!-- 我的页面头部 开始 -->
     <div class="my-header">
-      <!-- 用户信息开始 -->
-      <van-cell-group>
+      <!-- 用户信息 开始 -->
+      <!-- 登录状态 开始 -->
+      <van-cell-group v-if="isLogin">
         <van-cell title="单元格" center>
           <img
             slot="icon"
@@ -30,28 +31,53 @@
           </van-grid>
         </van-cell>
       </van-cell-group>
-      <!-- 用户信息结束 -->
-      <!-- 收藏 历史 开始 -->
-      <van-grid :column-num="2">
-        <van-grid-item icon="photo-o" text="收藏" />
-        <van-grid-item icon="photo-o" text="历史" />
-      </van-grid>
-      <!-- 收藏 历史 结束 -->
-      <!-- 小智同学 开始 -->
-      <van-cell title="小智同学" is-link to="/home" />
-      <!-- 小智同学 结束 -->
-      <!-- 退出登录 开始 -->
-      <van-cell value="退出登录" center />
-      <!-- 退出登录 借宿 -->
+      <!-- 登录状态 结束 -->
+      <div class="my-unlogin" v-else>
+        <div class="phone-login">
+          <img
+            src="https://i0.hdslb.com/bfs/album/1492be6ec64e6328e1fe16540cdf2dee65ae1c8f.png"
+          />
+        </div>
+        <div>点击登录</div>
+      </div>
+      <!-- 用户信息 结束 -->
     </div>
     <!-- 我的页面头部结束 -->
+    <!-- 收藏 历史 开始 -->
+    <van-grid :column-num="2">
+      <van-grid-item icon="photo-o" text="收藏" />
+      <van-grid-item icon="photo-o" text="历史" />
+    </van-grid>
+    <!-- 收藏 历史 结束 -->
+    <!-- 小智同学 开始 -->
+    <van-cell title="小智同学" is-link to="/home" />
+    <!-- 小智同学 结束 -->
+    <!-- 退出登录 开始 -->
+    <van-cell v-if="isLogin" value="退出登录" center />
+    <!-- 退出登录 借宿 -->
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MyIndex'
+  name: 'MyIndex',
+  data () {
+    return {
+      isLogin: false
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+/* 未登录页面样式 */
+.my-unlogin {
+  background-color: skyblue;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  width: 100%;
+}
+</style>
